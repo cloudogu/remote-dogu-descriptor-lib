37c95eb27360247ac5a65200bf0c0bf94000a601
Robert Auer <robert.auer@cloudogu.com>
2024-10-23 14:03:00 +0200

    Add functions with retry

--- repository/repository.go
+++ repository/repository.go
@@ -17,4 +17,10 @@ type RemoteDoguDescriptorRepository interface {
 	// ConnectionError if there are any connection issues
 	// Generic Error if there are any other issues
 	Get(context.Context, dogu.QualifiedDoguVersion) (*core.Dogu, error)
+	// GetLatestWithRetry returns the dogu descriptor for a dogu from the remote server
+	// and tries multiple times to reach the repository if errors occur.
+	GetLatestWithRetry(context.Context, dogu.QualifiedDoguName) (*core.Dogu, error)
+	// GetWithRetry returns a version specific dogu descriptor and
+	// tries multiple times to reach the repository if errors occur.
+	GetWithRetry(context.Context, dogu.QualifiedDoguVersion) (*core.Dogu, error)
 }
